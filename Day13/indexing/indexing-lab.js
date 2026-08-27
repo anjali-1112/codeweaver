6dfeee319413896f3fcdec0b8ec979489dc88c6a
Anjali Dayal <anjalidayal.kumari@gmail.com>
2026-08-27 15:07:08 +0530

    feat: add array indexing exercises

---
+++ Day13/indexing/indexing-lab.js
@@ -0,0 +1,14 @@
+// | Expression | Your Prediction | Actual Output |
+// |---|---|---|
+// | arr[0] (first element) | kamal|kamal |
+// | arr[2] (middle element) |hena | hena|
+// | arr[4] (last element) | faiz| faiz |
+// | arr[5] (invalid index) |undefined |undefined|
+
+const arr = ["kamal","vivek","hena","shanti","faiz"];
+console.log(arr[0]); //kamal
+console.log(arr[2]); //hena
+console.log(arr[4]); // faiz
+console.log(arr[5]); //undefined
+console.log(arr[arr.length]); // undefined
+
